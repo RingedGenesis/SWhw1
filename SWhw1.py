e32a4186e254708a13421d3139857b8808a3fd3a
Your Name <yubin5318@gmail.com>
2024-10-05 15:53:51 +0900

    곱셈 나눗셈 추가

--- SWhw1.py
+++ SWhw1.py
@@ -3,6 +3,10 @@ num2 = int(input("두 번째 숫자를 입력하세요: "))
 
 sum_result = num1 + num2
 diff_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2 if num2 != 0 else "0으로 나눌 수 없습니다."
 
 print(f"두 숫자의 합: {sum_result}")
 print(f"두 숫자의 차: {diff_result}")
+print(f"두 숫자의 곱: {product_result}")
+print(f"두 숫자의 나눗셈: {quotient_result}")
