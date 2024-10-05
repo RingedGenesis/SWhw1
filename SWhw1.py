9ac26407ea7a482319a1a081baef516466345dd6
Your Name <yubin5318@gmail.com>
2024-10-05 15:52:29 +0900

    덧셈 뺄셈 계산기

---
+++ SWhw1.py
@@ -0,0 +1,8 @@
+num1 = int(input("첫 번째 숫자를 입력하세요: "))
+num2 = int(input("두 번째 숫자를 입력하세요: "))
+
+sum_result = num1 + num2
+diff_result = num1 - num2
+
+print(f"두 숫자의 합: {sum_result}")
+print(f"두 숫자의 차: {diff_result}")
